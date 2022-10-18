85755a57893060ffa9d6bd25e3652512269ea34a
Thành Bùi <105998835+Thanh-Bui-97@users.noreply.github.com>
2022-10-18 20:18:52 +0700

    Config prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Button from '~/components/Button/index.js'
 
 function App() {
   return (
     <div className="App">
-      <Button />
       <h1>Hello</h1>
     </div>
   );
