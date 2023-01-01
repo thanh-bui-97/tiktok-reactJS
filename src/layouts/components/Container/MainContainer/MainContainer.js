a0b7a1675159612c8ef65f3c4f0da69c137beabd
Thành Bùi <105998835+Thanh-Bui-97@users.noreply.github.com>
2023-01-01 17:15:16 +0700

    Main-container: made effects for float group btns

--- src/layouts/components/Container/MainContainer/MainContainer.js
+++ src/layouts/components/Container/MainContainer/MainContainer.js
@@ -1,6 +1,7 @@
 // libraries
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import { useEffect, useRef } from 'react';
 // components
 import Button from '~/components/Button';
 import { UpToLineIcon } from '~/components/Icons';
@@ -9,10 +10,30 @@ import style from './MainContainer.module.scss';
 const cx = classNames.bind(style);
 
 function MainContainer({ children }) {
+  const floatBtnGroupRef = useRef('');
+
+  // float group buttons effects
+  useEffect(() => {
+    document.body.onscroll = () => {
+      const scrollPosition = window.scrollY;
+      console.log(scrollPosition);
+
+      if (scrollPosition > 0) {
+        floatBtnGroupRef.current.classList.remove(cx('hide-uptop-btn'));
+        floatBtnGroupRef.current.classList.add(cx('show-uptop-btn'));
+      } else {
+        setTimeout(() => {
+          floatBtnGroupRef.current.classList.remove(cx('show-uptop-btn'));
+          floatBtnGroupRef.current.classList.add(cx('hide-uptop-btn'));
+        }, 400);
+      }
+    };
+  }, []);
+
   return (
     <main className={cx('warpper')}>
       {children}
-      <div className={cx('float-btn-group')}>
+      <div ref={floatBtnGroupRef} className={cx('float-btn-group')}>
         <Button text router medium className={cx('download-btn')}>
           Get app
         </Button>
